762ac503f0956275ae748c6766be4b12af740b65
Ilham Rizki Hidayat <ilkuntwentyfive@gmail.com>
2021-03-16 13:33:05 +0700

    Lesson 16-20

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import { Link } from 'gatsby'
 import React from "react"
 import Layout from '../components/Layout'
 import styles from '../styles/home.module.css'
+import Img from "gatsby-image"
 
 export default function Home({ data }) {
   return (
@@ -13,8 +14,20 @@ export default function Home({ data }) {
           <p>Web Developer based in Cicurug</p>
           <Link className={styles.btn} to="/projects">My Project</Link>
         </div>
-        <img src="/1.jpg" alt="banner" style={{ maxWidth: '100%' }} />
+        <Img fluid={data.file.childImageSharp.fluid} />
       </section>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query Banner {
+    file(relativePath: {eq: "1.jpg"}) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -2,6 +2,7 @@ import { graphql, Link } from 'gatsby'
 import React from 'react'
 import Layout from '../../components/Layout'
 import styles from '../../styles/projects.module.css'
+import Img from 'gatsby-image'
 
 export default function Projects({ data }) {
     console.log(data)
@@ -17,6 +18,7 @@ export default function Projects({ data }) {
                     {articles.map(project => (
                         <Link to={"/projects/" + project.frontmatter.slug} key ={project.id}>
                             <div>
+                                <Img fluid={project.frontmatter.thumb.childImageSharp.fluid} />
                                 <h3>{ project.frontmatter.title }</h3>
                                 <p>{ project.frontmatter.stack }</p>
                             </div>
@@ -39,6 +41,13 @@ query ArticlePages {
             slug
             stack
             title
+            thumb {
+              childImageSharp {
+                fluid {
+                    ...GatsbyImageSharpFluid
+                }
+              }
+            }
           }
           id
         }
